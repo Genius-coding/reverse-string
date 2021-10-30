6bece914c5c9dbac685b88917a51c1debd69767d
Genius-coding <mohamed.boussarsar@outlook.com>
2021-10-30 13:27:31 +0200

    Reverse String

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reverse';
+  reversedText: string = '';
+
+
+  reveseString(event: Event) {
+    let message = (<HTMLInputElement>event.target).value;
+    console.log(message);
+    /* 
+      here we will take the text message and tranform it to an array of chars
+      after that we'll try to use some reactive functions to fix the problem
+      else the other way its the traditional way with for
+    */
+    this.reversedText = message.split("").reverse().join("");
+
+  }
+
+
+
 }
